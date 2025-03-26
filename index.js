423306e9dc95db1f7d68b8f5669b77d66a301f7c
+ <masaitaboi@gmail.com>
2025-03-27 02:38:46 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main");{
+main.remove();
+}
+const newHeader=document.createElement("H1");
+newHeader.id="victory"
+
+newHeader.innerHTML="<h1>victory</h1>masai is the champion"
